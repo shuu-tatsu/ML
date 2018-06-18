cfafaa816e7a151703dbb6cc962df5a257a4470a
tatsumi <helloshuushuu@gmail.com>
2018-06-18 12:02:50 +0900

    update

--- lr.py
+++ lr.py
@@ -8,54 +8,6 @@ import logging
 import random
 
 
-class Dataset(object):
-
-    def __init__(self, pos, neg):
-        self.pos_text = pos
-        self.neg_text = neg
-        self.pos_label = []
-        self.neg_label = []
-        self.mixed_dataset = []
-
-    def onehot_vec(self, word_number):
-        #initialize word_vec
-        word_vec = [0 for _ in range(200000)]
-        word_vec[int(word_number)] = 1
-        return word_vec
-
-    def vectorizer(self, line_include_freq_info):
-        list_word_vec = [self.onehot_vec(i.split(':')[0]) for i in line_include_freq_info]
-        vec_line = np.sum(list_word_vec, axis=0)
-        return vec_line
-
-    def load(self, dataset):
-        with open(dataset, 'r') as r:
-            lines = [i for i in r.read().split('\n')]
-            lines = [i.split() for i in lines[:-1]]
-            vec_lines = [self.vectorizer(i) for i in lines]
-            return vec_lines
-
-    def labeling(self):
-        pos_lines = self.load(self.pos_text)
-        self.pos_label = [(line, 1) for line in pos_lines]
-        neg_lines = self.load(self.neg_text)
-        self.neg_label = [(line, 0) for line in neg_lines]
-
-    def merge(self):
-        self.mixed_dataset = self.pos_label + self.neg_label
-        return self.mixed_dataset
-
-
-def load(dataset):
-    train_set = Dataset(dataset[0], dataset[1])
-    train_set.labeling()
-    train_set.merge()
-    random.shuffle(train_set.mixed_dataset)
-    xs_train = [i[0] for i in train_set.mixed_dataset]
-    ys_train = [i[1] for i in train_set.mixed_dataset]
-    return xs_train, ys_train
-
-
 class GaussianInitializer(object):
     def __init__(self):
         self.dim = 0
@@ -120,8 +72,10 @@ def get_mini_batches(xs_train, ys_train):
     for i in range(len(ys_train)):
         yield xs_train[i], ys_train[i]
 
-def train(train_set, epochs, learning_rate):
-    xs_train, ys_train = load(train_set)
+
+def train(xs_train, ys_train, epochs, learning_rate):
+    xs_train = np.load(xs_train)
+    ys_train = np.load(ys_train)
     dim = xs_train[0].shape[0]
     w = np.empty((dim,), dtype=np.float16)
     initializer = GaussianInitializer()
@@ -146,30 +100,28 @@ def train(train_set, epochs, learning_rate):
             "[{}] epoch {} - #samples: {}, loss: {:.8f}, accuracy: {:.8f}"
             .format("train", epoch, len(ys_train), loss, accuracy))
 
-def test(test_set):
-    test_set = None
+
+def test(xs_test, ys_test):
+    xs_test = None
+    ys_test = None
 
 
-def main(train_set, test_set, epochs, learning_rate):
-    train(train_set, epochs, learning_rate)
-    test(test_set)
+def main(xs_train, ys_train, xs_test, ys_test, epochs, learning_rate):
+    train(xs_train, ys_train, epochs, learning_rate)
+    test(xs_test, ys_test)
 
 
 if __name__ == '__main__':
     logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO)
-
-    train_pos_set = 'positive.review'
-    train_neg_set = 'negative.review'
+    epochs = 50
+    learning_rate = 0.01
     """
-    train_pos_set = 'pos_train.review'
-    train_neg_set = 'neg_train.review'
+    xs_train = 'xs_train.npy'
+    ys_train = 'ys_train.npy'
     """
-    test_pos_set = 'pos_test.review'
-    test_neg_set = 'neg_test.review'
-    epochs = 10
-    learning_rate = 0.1
-
-    train_set = [train_pos_set, train_neg_set]
-    test_set =  [test_pos_set, test_neg_set]
-    main(train_set, test_set, epochs, learning_rate)
+    xs_train = 'xs_all_train.npy'
+    ys_train = 'ys_all_train.npy'
+    xs_test = 'xs_train.npy'
+    ys_test = 'ys_train.npy'
+    main(xs_train, ys_train, xs_test, ys_test, epochs, learning_rate)
