8dd5c87de82b11dbb25f133b7596758cc401559b
tatsumi <helloshuushuu@gmail.com>
2018-07-05 21:37:54 +0900

    update

--- neural_network/load.py
+++ neural_network/load.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 class DataLoader():
-    def __init__(self, data_path, batch_size, shuffle):
+
+    def __init__(self, data_path, shuffle):
         self.data_path = data_path
-        self.batch_size = batch_size
         self.shuffle = shuffle
 
     def load(self):
@@ -13,25 +14,25 @@ class DataLoader():
             data_features, data_labels = read_data(lines)
         return data_features, data_labels
 
+
 def read_data(lines):
     features = [line[:-1].split(',')[1:] for line in lines]
     labels = [line.split(',')[0] for line in lines]
     return features, labels
 
-def main(train_data_path, test_data_path):
-    train_loader = DataLoader(train_data_path, batch_size=4, shuffle=True)
+def main():
+    #FILE_TRAIN = './mnist/MNIST-csv/train.csv'
+    #FILE_TEST = './mnist/MNIST-csv/test.csv'
+    FILE_TRAIN = './mnist/MNIST-csv/toy_train.csv'
+    FILE_TEST = './mnist/MNIST-csv/toy_test.csv'
+
+    train_loader = DataLoader(FILE_TRAIN, shuffle=False)
     train_features, train_labels = train_loader.load()
 
-    test_loader = DataLoader(test_data_path, batch_size=4, shuffle=False)
+    test_loader = DataLoader(FILE_TEST, shuffle=False)
     test_features, test_labels = test_loader.load()
 
-    print(test_features)
-    print(test_labels)
-    
+    print(train_features, train_labels)
 
 if __name__ == '__main__':
-    #train_data_path = './mnist/MNIST-csv/train.csv'
-    #test_data_path = './mnist/MNIST-csv/test.csv'
-    train_data_path = './mnist/MNIST-csv/toy_train.csv'
-    test_data_path = './mnist/MNIST-csv/toy_test.csv'
-    main(train_data_path, test_data_path)
+    main()
